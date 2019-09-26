7b7f20b20ce3a3ebbf9d1775f5359665a3de7f29
DangerousVegetable <mukoseev.l@yandex.ru>
2019-09-27 00:41:26 +0300

    BUG fixed

--- 9th/FILES/25.09.2019 HW/pr9.ml
+++ 9th/FILES/25.09.2019 HW/pr9.ml
@@ -13,7 +13,7 @@ let to_cp1251 l =
 		|n::tl -> (0b00111111 land n) lor ((f tl) lsl 6) in
 	f (List.rev l);; 
 
-let rec from_cp1251_to_utf8 n = if n >= in_channel_length in_chan then (close_in in_chan; close_out out_chan) else (let byte = input_byte in_chan in List.iter (fun x -> output_byte out_chan x) (to_utf8 byte); from_cp1251_to_utf8 (n+1));;
+let rec from_cp1251_to_utf8 n = if n >= in_channel_length in_chan then (close_in in_chan; close_out out_chan) else (let byte = input_byte in_chan in if byte < 128 then List.iter (fun x -> output_byte out_chan x) (to_utf8 byte) else List.iter (fun x -> output_byte out_chan x) (to_utf8 (byte+848)); from_cp1251_to_utf8 (n+1));;
 
 let rec from_utf8_to_cp1251 n = 
 	
@@ -21,9 +21,10 @@ let rec from_utf8_to_cp1251 n =
 
 	let rec read k = if k <= 0 then [] else (input_byte in_chan)::(read (k-1)) in
 
-	if n >= in_channel_length in_chan then (close_in in_chan; close_out out_chan) else let byte = input_byte in_chan in (); let nz = f byte in if nz = 0 then (output_byte out_chan byte; from_utf8_to_cp1251 (n+1)) else (output_byte out_chan (to_cp1251 (byte::(read ((f byte)-1)))); from_utf8_to_cp1251 (n+(f byte)));; 
+	if n >= in_channel_length in_chan then (close_in in_chan; close_out out_chan) else let byte = input_byte in_chan in (); let nz = f byte in if nz = 0 then (output_byte out_chan byte; from_utf8_to_cp1251 (n+1)) else ((output_byte out_chan (to_cp1251 (byte::(read ((f byte)-1)))-848)); from_utf8_to_cp1251 (n+(f byte)));; 
 
 (*from_cp1251_to_utf8 0;;*) 
+(*print_int (to_cp1251 [208;144]);;*)
 from_utf8_to_cp1251 0;;
 	  
 

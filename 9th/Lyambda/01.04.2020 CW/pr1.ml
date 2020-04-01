b1c731e4ecd0b26f89b8a43fba3f6814024f7bec
DangerousVegetable <mukoseev.l@yandex.ru>
2020-04-01 12:53:30 +0300

    Классная работа на 01.04.2020

---
+++ 9th/Lyambda/01.04.2020 CW/pr1.ml
@@ -0,0 +1,8 @@
+1) S K K ->> \z.((K z) (K z)) ->> \z.z
+2) K (S I) K ->> S I = \y.\z.((I z) (y z)) -> \y.\z.(z (y z)) 
+3) F = K I?
+   K I = (\x.\y.x) (\t.t) -> \y.\t.t = \x.\y.y
+4) 1 = S F?
+   S F = (\x.\y.\z.(x z (y z))) F -> \y.\z.(F z (y z)) ->> \y.\z.(y z) = \f.\g.(f g)
+5) T = K
+

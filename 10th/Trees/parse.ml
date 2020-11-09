91d7dacfa1cadc79882ac35decf5a378106ea34d
DangerousVegetable <mukoseev.l@yandex.ru>
2020-11-09 11:05:20 +0300

    КР за 09.11.2020

--- 10th/Trees/parse.ml
+++ 10th/Trees/parse.ml
@@ -1,14 +1,14 @@
 type tree = Tree of tree list;;
 
 let tree_of_string s = 
-	let rec parse n = 
-		if n >= String.length s then ([],n) else
-			match s.[n] with
-			|'(' -> let (tl1,n) = parse (n+1) in if s.[n] = ')' then let (tl2,n) = parse (n+1) in ((Tree tl1)::tl2,n) else failwith ((string_of_int n) ^ " - unexpected \')\'")
-			|')' -> ([],n)
-			|_ -> failwith "Unknown symbol" in
+	let s = s^")" in
+	let rec parse n =      
+		match s.[n] with
+		|'(' -> let (tl1,n) = parse (n+1) in if s.[n] = ')' then let (tl2,n) = parse (n+1) in ((Tree tl1)::tl2,n) else failwith ((string_of_int n) ^ " - unexpected \')\'")
+		|')' -> ([],n)
+		|_ -> failwith "Unknown symbol" in
 	let (t,n) = parse 0 in 
-	if n = String.length s then t else failwith ("Incorrect tree: "^(string_of_int n));;
+	if n = (String.length s)-1 then t else failwith ("Incorrect tree: "^(string_of_int n));;
 
 let rec string_of_tree tl = 
 	match tl with

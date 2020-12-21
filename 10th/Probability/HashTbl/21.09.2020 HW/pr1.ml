d260b869b4708a2fafedabeaae9534a8abc963ad
DangerousVegetable <mukoseev.l@yandex.ru>
2020-12-21 23:03:16 +0300

    ДЗ на 21.09.2020

--- 10th/Probability/HashTbl/21.09.2020 HW/pr1.ml
+++ 10th/Probability/HashTbl/21.09.2020 HW/pr1.ml
@@ -1,6 +1,6 @@
 let n = read_int();;
-
-let l = List.map (fun x -> (x*x) mod 10) [0;1;2;3;4;5;6;7;8;9];;
+let m = read_int();;
+let l = List.map (fun x -> (x*x) mod 10) (List.init (int_of_float(sqrt (float_of_int n))) (fun x -> x+1));;
 
 (*let rec f l x n =
 	match l with
@@ -9,7 +9,7 @@ let l = List.map (fun x -> (x*x) mod 10) [0;1;2;3;4;5;6;7;8;9];;
 
 let p = f l 0 0;;
 *)
-print_float ((float_of_int(List.fold_left (fun s x -> if x = n then s+1 else s) 0 l))/.(float_of_int (List.length l)));;
+print_float ((float_of_int(List.fold_left (fun s x -> if x = m then s+1 else s) 0 l))/.(float_of_int (List.length l)));;
 
 
 

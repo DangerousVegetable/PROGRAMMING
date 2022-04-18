df0a87cd532ae405f751d5ef46d93386a9c861d7
DangerousVegetable <mukoseev.l@yandex.ru>
2022-04-18 18:03:01 +0300

    Наивные философы с визуализацией.

--- 11th/Сортировки и Многопоточность/25.03.2022 HW/ph.py
+++ 11th/Сортировки и Многопоточность/25.03.2022 HW/ph.py
@@ -1,26 +1,54 @@
 import threading
-import multiprocessing
+import queue
 import time
 import sys
+import os
 
 forks = [threading.Lock() for i in range(7)]
+
 transaction_counter = 0
 transaction_lock = threading.Lock()
+state = [-1,-1,-1,-1,-1,-1,-1]
+
+qs = queue.Queue()
+
+def drawtable(tb):
+    l = 4*6
+    print((" "+"_"*(l//2-1) if tb[0] == 6 else " "*(l//2))+"7"+("_"*(l//2-1)+" " if tb[6] == 6 else " "*(l//2)))
+    print(("/" if tb[0] == 6 else " ")+" "*(l-1)+("\\" if tb[6] == 6 else " "))
+    for i in range(7):
+        print("#   ", end = '')
+    print("")
+
+    for i in range(6):
+        print((" \\ " if tb[i] == i else "   ")+("/" if tb[i+1] == i else " "), end = '')
+    print("")
+
+    for i in range(6):
+        print("  "+str(i+1)+" ", end = '')
+    print("")
 
 def take_fork(n,f,do_take):
     global transaction_counter
     global transaction_lock
-    transaction_lock.acquire()
-
-    print("Transaction %d: filosof %d %s left fork %d" % (transaction_counter, n, ("takes" if do_take else "returns"), f))
-    sys.stdout.flush()
-    transaction_counter += 1
-    transaction_lock.release()
+    global state
+    global qs
 
     if do_take:
         forks[f].acquire()
     else:
         forks[f].release()
+    
+    with transaction_lock:
+        state[f] = n if do_take else -1
+        
+        os.system("cls")
+        print("Transaction %d:" % transaction_counter)
+        print(state)
+        drawtable(state)
+        print("")
+
+        transaction_counter += 1
 
 def naiveFilosofLife(num):
     left = num
@@ -28,17 +56,31 @@ def naiveFilosofLife(num):
     while True:
         take_fork(num,left,True)
         take_fork(num,right,True)
-        print("Filosof %d: eating" % num)
-        sys.stdout.flush()
+        #print("Filosof %d: eating" % num)
         take_fork(num,left,False)
         take_fork(num,right,False)
-        print("Filosof %d: thinking about philosopy" % num)
+        #print("Filosof %d: thinking about philosopy" % num)
+
+
+def observer(): #optional solution
+    global qs
+
+    while True:
+        table = qs.get()
+
+        os.system('cls')
+        print("Transaction %d:" % table[0])
+        print(table[1])
+        drawtable(table[1])
+        print("")
         sys.stdout.flush()
+
+        qs.task_done()
 
 th = [threading.Thread(target = naiveFilosofLife, args=(i,)) for i in range(7)]
 
 for t in th:
     t.start()
-#e1.set()
-for t in th:
-    t.join()
+
+#threading.Thread(target = observer).start()
+
